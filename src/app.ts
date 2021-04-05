a5d6c814270c28b4df08cc58c56a7551ef87f4ba
Haydn Wiese <haydnwiese@gmail.com>
2021-04-05 14:27:42 -0400

    Add basic knex implementation

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,36 @@
 import express, {Application, Request, Response} from 'express';
+import { Knex, knex } from 'knex';
 
 const app: Application = express();
-
 const port: number = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello world');
+const config: Knex.Config = {
+    client: 'mysql2',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        database: 'activity_sharing'
+    }
+}
+const knexInstance = knex(config);
+
+interface User {
+    id: number;
+    authId: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    registeredAt: string;
+    lastLogin: string;
+}
+
+app.get('/', async (req: Request, res: Response) => {
+    try {
+        const users = await knexInstance<User>('user').select('*');
+        res.send(users)
+    } catch (err) {
+        console.error(err)
+    }
 });
 
 app.listen(port, () => {
