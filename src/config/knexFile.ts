0c8e14cd5e518aaedef136ce0e7cf20b15443176
Haydn Wiese <haydnwiese@gmail.com>
2021-04-20 21:27:50 -0400

    Add date formatting to db client configuration

--- src/config/knexFile.ts
+++ src/config/knexFile.ts
@@ -1,8 +1,16 @@
+import moment from 'moment';
+
 export default {
     client: 'mysql2',
     connection: {
         host: '127.0.0.1',
         user: 'root',
-        database: 'activity_sharing'
+        database: 'activity_sharing',
+        typeCast: function (field: any, next: any) {
+            if (field.type == 'DATETIME') {
+                return moment(field.string()).format('YYYY-MM-DD[T]HH:mm:ss');
+            }
+            return next();
+        }
     }
 }
